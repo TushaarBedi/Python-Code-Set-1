a38604f0a82b9162c63964448f7c971c36f104e0
Tushaar Bedi <tushaar@gmail.com>
2018-03-08 18:01:55 -0500

    Updated/Modified my PyParagraph_Tushaar_Py.py file

--- PyParagraph_Tushaar_Py.py
+++ PyParagraph_Tushaar_Py.py
@@ -25,7 +25,8 @@ Paragraph_Count = 0
 
 # Open the text file
 with open(Input_File_Data_Path) as txtfile:
-    paragraph = txtfile.read()
+    paragraph = txtfile.read() #reading the input file and stroing it in the paragrpah variable
+    stringpara = str(paragraph) #creating a string version of the variable to store this as string
 
 
     Sentence_1 = paragraph.split(".") #Store sentences that are seperated by periods as an array
@@ -33,12 +34,11 @@ with open(Input_File_Data_Path) as txtfile:
     Sentence_3 = paragraph.split("?") #Store sentences that are seperated by question marks as an array
     Word = paragraph.split(" ") #Store all the independent words in the whole text file containing one paragraph as an array
     
-
+  
 
 Word_Count = len(Word) #get the lenght of the word arrays to calculate approximate independent words
 Sentence_Count = len(Sentence_1) - 1 + len(Sentence_2) - 1 + len(Sentence_3) - 1 #Calculate the number of sentences in the paragraph which are seperated by . ! ?
-Letter_Count = len (paragraph) #Count all characters in the paragraph. Ideally we should then 'substract' the .,!?""'' etc. In this code we are only returning 'approximates', so we are not writing that code logic
-    
+Letter_Count = len(re.findall('[A-z]', stringpara))#Count all alphabets in the paragraph.
 Average_Letter_Count = Letter_Count / Word_Count #Simple mathematical formula for average letter count per each word
 Average_Sentence_Length = Word_Count/Sentence_Count #Simple mathematical forumula for average sentence length in terms of number of words per sentence
 
